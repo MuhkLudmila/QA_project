d3694c46d34864ae22b8b143e96780dbf2062b69
MushkLudmila <mushkludmila@gmail.com>
2022-03-30 15:30:35 +0300

    add HW50

---
+++ src/New_Package/My/HW50.java
@@ -0,0 +1,7 @@
+package New_Package.My;
+
+public class HW50 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/New_Package/My/main.java
+++ src/New_Package/My/main.java
@@ -1,2 +1,13 @@
-package New_Package.My;public class main {
+package New_Package.My;
+
+public class main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 4;
+        boolean c = a < b;
+        System.out.println(c);
+        System.out.println(a);
+
+    }
 }
