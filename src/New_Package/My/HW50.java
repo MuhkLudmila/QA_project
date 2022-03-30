d3fc42f87136ae5ecd6de250bf595bb1877eada7
MushkLudmila <mushkludmila@gmail.com>
2022-03-30 20:12:25 +0300

    add HW50

--- src/New_Package/My/HW50.java
+++ src/New_Package/My/HW50.java
@@ -2,6 +2,12 @@ package New_Package.My;
 
 public class HW50 {
     public static void main(String[] args) {
-
+        int n;
+        n = 9;
+        if(n%2==0) {
+            System.out.println(n+" - chetnoe chislo");
+        } else {
+            System.out.println(n+" - nechetnoe chislo");
+        }
     }
 }
